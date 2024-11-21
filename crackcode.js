9b01d50673b24c339c082b50173fb142abb2ba5c
Ken-LeParisienDeLEnfer <ken.ellison.4@gmail.com>
2024-11-21 12:20:48 +1100

    first commit

---
+++ crackcode.js
@@ -0,0 +1,44 @@
+const map = [{
+    letter: 'a',
+    sibblings: ['a', 'b', 'd']
+},
+{
+    letter: 'b',
+    sibblings: ['a', 'b', 'c', 'e']
+},
+{
+    letter: 'c',
+    sibblings: ['b', 'c', 'f']
+},
+{
+    letter: 'd',
+    sibblings: ['a', 'e', 'd']
+},
+{
+    letter: 'e',
+    sibblings: ['b', 'd', 'e', 'f', 'h']
+},
+{
+    letter: 'f',
+    sibblings: ['f', 'c', 'e','i']
+},
+{
+    letter: 'g',
+    sibblings: ['g', 'd', 'h']
+},
+{
+    letter: 'h',
+    sibblings: ['e', 'h', 'g', 'i', 'j']
+},
+{
+    letter: 'i',
+    sibblings: ['f', 'h', 'i']
+},
+{
+    letter: 'j',
+    sibblings: ['j', 'h']
+}]
+
+function crackCode(code) {
+
+}
